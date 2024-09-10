3f693ddd0f9937091d847642acf9e2e53c409203
JustinoLucas <justinolucas@hotmail.com>
2024-09-10 13:20:15 -0300

    Add project files.

---
+++ TiposDados1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Declarando variáveis numéricas");
+Console.WriteLine();
+// Declarando Variáveis
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+valor3 = 7777;
+uint valor4 = 2147483647;
+long valor5 = -214748364792;
+
+const int valor6 = 999;
+//valor6 = 777;
+
+int var1=8, var2=1, var3=12;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+
+Console.ReadLine();
+
