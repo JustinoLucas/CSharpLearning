8b24014319c48a062a938cc4f57172dd2fe0c3f3
JustinoLucas <justinolucas@hotmail.com>
2024-09-11 15:21:55 -0300

    Nullable Types

---
+++ NullableTypes1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+Console.WriteLine("== Nullable Types ==\n");
+
+//Nullable<int> i = null;
+//Nullable<double> d = null;
+//Nullable<bool> b = null;
+
+//int? i = null;
+//double? d = null;
+//bool? b = null;
+
+//int? a = null;
+//int b = a ?? 0;
+
+//Console.WriteLine(b);
+
+
+//Console.WriteLine(i);
+//Console.WriteLine(d);
+//Console.WriteLine(b);
+
+
+//int? x = 4;
+//int? y = 3;
+//int? z = x * y;
+
+//Console.WriteLine(z); 
+
+
+int? b = 100;
+if (b.HasValue)
+{
+    Console.WriteLine($"b = {b.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui um valor (null)");
+}
+
+
+Console.ReadLine();
