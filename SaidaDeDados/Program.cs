174d03db774d7f70cb21af9d24a54351fba667fa
JustinoLucas <justinolucas@hotmail.com>
2024-09-17 14:23:03 -0300

    Formatação de saida de dados 1

---
+++ SaidaDeDados/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Saida de Dados : Formatação");
+Console.WriteLine();
+
+int idade = 25;
+string nome = "Maria";
+
+Console.Write(nome);
+Console.Write(idade);
+Console.WriteLine();
+
+// Escrevendo na mesma linha
+Console.Write(nome);
+Console.Write(" tem ");
+Console.Write(idade);
+Console.Write(" anos\n");
+Console.WriteLine("------Concatenação------");
+
+// Usando concatenação : usando o +
+Console.WriteLine(nome + " tem " + idade + " anos");
+Console.Write(nome + " tem " + idade + " anos\n");
+Console.WriteLine("------Interpolação------");
+
+// Usando a interpolação de strings :  $ -> a interpolação {}
+Console.WriteLine($"{nome} tem {idade} anos\n");
+Console.WriteLine("------Place Holders------");
+
+// Usando place holders : usa {} com numeração com inicio em zero
+Console.WriteLine("{0} tem {1} anos", nome, idade);
+
+Console.ReadLine();
