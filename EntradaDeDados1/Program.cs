f379fc7bc7f04ff7cfb21892a0b8dd3065d9fb2c
JustinoLucas <justinolucas@hotmail.com>
2024-09-17 16:11:00 -0300

    Entrada de Dados console

---
+++ EntradaDeDados1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("\n -- Entrada de Dados --");
+
+Console.WriteLine("\nInforme o seu nome");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine("O seu nome é " + nome);
+
+Console.WriteLine("\nInforme o sua idade");
+
+int idade = Convert.ToInt32(Console.ReadLine());
+int idade2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"\nSua idade é {idade}");
+Console.WriteLine($"\nSua idade é {idade2}");
+
+Console.ReadKey();
