15aff2c0d26dac56bda708b2bd1703954878c7ae
JustinoLucas <justinolucas@hotmail.com>
2024-09-19 12:51:36 -0300

    Inferencia de Tipos

---
+++ InferenciaTipos1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("-- Inferência de Tipos (var) --\n");
+
+var idade = 25;
+var nome = "Maria";
+var salario = 2500.00m;
+
+Console.WriteLine($"{nome} tem {idade} anos e ganha {salario.ToString("c")}");
+
+// Var limitações
+
+var salario = null;
+var titulo;
+var salario, imposto, total;
+
+// Não posso mudar o tipo apos inicializar
+
+var num = 10;
+num = num + 20;
+num = "teste";
+
+
+
+Console.ReadKey();
+
+var teste = new Teste();
+
+teste.MeuMetodo();
+class Teste
+{
+    public void MeuMetodo()
+    {
+        Console.WriteLine("Meu metodo");
+    }
+}
