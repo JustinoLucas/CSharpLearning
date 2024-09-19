815e8fdcd91492023fae1605c3184a1f9abb9d28
JustinoLucas <justinolucas@hotmail.com>
2024-09-19 13:00:52 -0300

    Operadores de Atribuição Numerico e String

---
+++ OperadoresAtribuicao1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("-- Operadores de Atribuição --\n");
+
+// Usando operadores de atribuição com tipos numericos
+
+int x = 10;
+
+Console.WriteLine($"Valor inicial de x = {x}");
+Console.WriteLine($"x += 5 -> {x+=5}");
+Console.WriteLine($"x -= 3 -> {x-=3}");
+Console.WriteLine($"x *= 4 -> {x*=4}");
+Console.WriteLine($"x /= 5 -> {x/=5}");
+Console.WriteLine($"x %= 5 -> {x%=5}");
+
+// Usando operadoes + e += com tipos de string
+
+var y = "123";
+
+Console.WriteLine($"Valor inicial de y = {y}");
+Console.WriteLine($"y += \"456\" = {y+="456"}");
+
+Console.ReadKey();  
