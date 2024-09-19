0b93ff824ac3e0c182586486ad8981decfa0e69b
JustinoLucas <justinolucas@hotmail.com>
2024-09-19 14:17:12 -0300

    Operador Relacional

---
+++ OperadoresRelacionais1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("-- Operadores Relacionais --\n");
+
+int x = 10;
+int y = 20;
+
+Console.WriteLine($"Valor de X {x}");
+Console.WriteLine($"Valor de Y {y}");
+
+bool resultado = x == y;
+Console.WriteLine(x == y);
+Console.WriteLine(x > y);
+Console.WriteLine(x < y);
+Console.WriteLine(x >= y);
+Console.WriteLine(x <= y);
+Console.WriteLine(x != y);
+
+
+
+Console.ReadLine();
